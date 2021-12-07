5dc70da450cae98e2d7db30c865ec94acf77feb0
Eric Cousineau <eric.cousineau@tri.global>
2021-12-07 13:43:12 -0500

    try fewer episodes

--- ibc/configs/particle/collect_data.sh
+++ ibc/configs/particle/collect_data.sh
@@ -2,9 +2,9 @@
 
 python3 ibc/data/policy_eval.py -- \
  --alsologtostderr \
- --num_episodes=200 \
+ --num_episodes=10 \
  --policy=particle_green_then_blue \
  --task=PARTICLE \
  --dataset_path=ibc/data/particle/2d_oracle_particle.tfrecord \
- --replicas=10  \
+ --replicas=1  \
  --use_image_obs=False
