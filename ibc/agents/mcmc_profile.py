e076f33886eec9eb2fe542c3f7982f3d833bcea8
Eric Cousineau <eric.cousineau@tri.global>
2021-12-20 14:51:41 -0500

    simplify; this is on-par w/ torch

--- ibc/agents/mcmc_profile.py
+++ ibc/agents/mcmc_profile.py
@@ -3,39 +3,27 @@ import time
 import numpy as np
 import tensorflow.compat.v2 as tf
 
-from ibc.ibc.agents import mcmc
 from ibc.ibc.utils import debug
 
 
-class SimpleMlp(tf.keras.Model):
-  def __init__(self, hidden_sizes):
-    super().__init__()
-
-    self._fc_layers = []
-    for l in range(len(hidden_sizes)):
-      self._fc_layers.append(tf.keras.layers.Dense(hidden_sizes[l]))
-    self._final = tf.keras.layers.Dense(1)
-
-  def _mlp(self, x, training):
-    for l in range(len(self._fc_layers)):
-      x = self._fc_layers[l](x, training=training)
-    x = self._final(x, training=training)
-    return x
-
-  def call(self, x, training, step_type=(), network_state=()):
-    """Mock network."""
-    return self._mlp(x, training), ()
+def make_mlp(hidden_sizes):
+  net = tf.keras.Sequential()
+  for hidden_size in hidden_sizes:
+    net.add(tf.keras.layers.Dense(hidden_size))
+  net.add(tf.keras.layers.Dense(1))
+  return net
 
 
+# @tf.function
 def gradient(f, x):
   with tf.GradientTape() as g:
     g.watch(x)
-    f_value, _ = f(x, training=False)
+    f_value = f(x, training=False)
   df_dx = g.gradient(f_value, x)
   return df_dx
 
 
-@tf.function
+# @tf.function
 def fake_langevin(net, yhs, num_iter):
   out = tf.zeros_like(yhs)
   for _ in range(num_iter):
@@ -52,7 +40,7 @@ def test_profile():
   yhs = np.random.rand(N * L, DimY).astype(np.float32)
   yhs = tf.convert_to_tensor(yhs)
 
-  net = SimpleMlp([256, 256])
+  net = make_mlp([256, 256])
   net(yhs)
   print(net.summary())
 
@@ -63,6 +51,7 @@ def test_profile():
   for _ in range(count + 1):
 
     out = fake_langevin(net, yhs, num_iter)
+    out = out.numpy()
 
     if t_start is None:
       t_start = time.time()
@@ -74,7 +63,7 @@ def test_profile():
 
 @debug.iex
 def main():
-  # tf.config.run_functions_eagerly(True)
+  tf.config.run_functions_eagerly(True)
   test_profile()
 
 
