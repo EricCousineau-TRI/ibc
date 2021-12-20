1b6c4f95c86719ee1d63e23b058e8dd210145837
Eric Cousineau <eric.cousineau@tri.global>
2021-12-20 14:24:55 -0500

    further simplify

--- ibc/agents/mcmc_profile.py
+++ ibc/agents/mcmc_profile.py
@@ -5,81 +5,68 @@ import tensorflow.compat.v2 as tf
 
 from ibc.ibc.agents import mcmc
 from ibc.ibc.utils import debug
-from ibc.networks.layers.mlp_dropout import MLPDropoutLayer
 
 
-class EnergyNet(tf.keras.Model):
-  def __init__(self, mlp):
-    super(EnergyNet, self).__init__()
-    self.mlp = mlp
+class SimpleMlp(tf.keras.Model):
+  def __init__(self, hidden_sizes):
+    super().__init__()
 
-  def call(self, inputs, training, step_type=(), network_state=()):
+    self._fc_layers = []
+    for l in range(len(hidden_sizes)):
+      self._fc_layers.append(tf.keras.layers.Dense(hidden_sizes[l]))
+    self._final = tf.keras.layers.Dense(1)
+
+  def _mlp(self, x, training):
+    for l in range(len(self._fc_layers)):
+      x = self._fc_layers[l](x, training=training)
+    x = self._final(x, training=training)
+    return x
+
+  def call(self, x, training, step_type=(), network_state=()):
     """Mock network."""
-    _, actions = inputs
-    return self.mlp(actions), ()
+    return self._mlp(x, training), ()
+
+
+def gradient(f, x):
+  with tf.GradientTape() as g:
+    g.watch(x)
+    f_value, _ = f(x, training=False)
+  df_dx = g.gradient(f_value, x)
+  return df_dx
 
 
 @tf.function
-def fake_langevin(energy_network, obs, init_action_samples, num_iter):
-  out = None
+def fake_langevin(net, yhs, num_iter):
+  out = tf.zeros_like(yhs)
   for _ in range(num_iter):
-    de_dact = mcmc.gradient_wrt_act(
-      energy_network,
-      obs,
-      init_action_samples,
-      training=False,
-      network_state=(),
-      tfa_step_type=(),
-      apply_exp=False)
-    if out is None:
-      out = de_dact
-    else:
-      out += de_dact
+    de_dact = gradient(net, yhs)
+    out += de_dact
   return out
 
 
 def test_profile():
+  N = 32
+  L = 8
+  DimY = 2
 
-  dense = tf.keras.layers.Dense
-  mlp = MLPDropoutLayer(
-      [512, 512, 1],
-      rate=0,
-      kernel_initializer='normal',
-      bias_initializer='normal',
-      dense=dense,
-  )
-  energy_network = EnergyNet(mlp)
-  # Create stuff...
-  energy_network(((), np.random.randn(1, 2).astype(np.float32)))
+  yhs = np.random.rand(N * L, DimY).astype(np.float32)
+  yhs = tf.convert_to_tensor(yhs)
 
-  batch_size = 32
-  num_action_samples = 8
-
-  obs = ()
-  init_action_samples = np.random.rand(batch_size * num_action_samples,
-                                       2).astype(np.float32)
-
-  print()
+  net = SimpleMlp([256, 256])
+  net(yhs)
+  print(net.summary())
 
   count = 10
   num_iter = 100
 
   t_start = None
   for _ in range(count + 1):
-    out = fake_langevin(energy_network, obs, init_action_samples, num_iter)
-    # action_samples = mcmc.langevin_actions_given_obs(
-    #     energy_network,
-    #     obs,
-    #     init_action_samples,
-    #     policy_state=(),
-    #     min_actions=np.array([0., 0.]).astype(np.float32),
-    #     max_actions=np.array([1., 1.]).astype(np.float32),
-    #     training=False,
-    #     num_iterations=num_iter,
-    #     num_action_samples=num_action_samples,
-    #     tfa_step_type=())
+
+    out = fake_langevin(net, yhs, num_iter)
+
     if t_start is None:
       t_start = time.time()
+
   dt = time.time() - t_start
   print(dt / count)
   print("\n\n")
@@ -87,6 +74,7 @@ def test_profile():
 
 @debug.iex
 def main():
+  # tf.config.run_functions_eagerly(True)
   test_profile()
 
 
