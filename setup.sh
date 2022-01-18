98d3762dd28861dd83a3eb37b318005d5f1a63e6
Eric Cousineau <eric.cousineau@tri.global>
2022-01-18 13:31:51 -0500

    for desktop

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ _parent_dir=$(cd $(dirname ${BASH_SOURCE})/.. && pwd)
 
 source ${_parent_dir}/ibc_venv/bin/activate
 export PYTHONPATH=${_parent_dir}
-# export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=1
 export TF_CPP_MIN_LOG_LEVEL=2
