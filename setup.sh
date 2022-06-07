22a9ee0e2d77bef67064beb4770096daf23e8787
Eric Cousineau <eric.cousineau@tri.global>
2022-06-07 18:55:21 -0400

    oops

--- setup.sh
+++ setup.sh
@@ -4,5 +4,4 @@ _parent_dir=$(cd $(dirname ${BASH_SOURCE})/.. && pwd)
 
 source ${_parent_dir}/ibc/venv/bin/activate
 export PYTHONPATH=${_parent_dir}
-export CUDA_VISIBLE_DEVICES=1
 export TF_CPP_MIN_LOG_LEVEL=2
